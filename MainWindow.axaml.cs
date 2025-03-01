dbe52fe81c2c6aeae67d7a0da9d36cad9bd3244a
fistr24@student.sdu.dk <fistr24@student.sdu.dk>
2025-03-01 19:05:44 +0100

    Moved functions from MainWindow to MyCanvas

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -13,40 +13,16 @@ namespace AvaloniaApplication1;
 
 public partial class MainWindow : Window
 {
-    private IBrush? color = Brushes.Black;
-    private int[,] image;
-    private int width;
-    private int height;
-    private int pixelSize;
+    
     public MainWindow()
     {
         InitializeComponent();
-        width = 10;
-        height = 10;
-        pixelSize = 20;
-        image = new int[height, width];
-
-        UpdatePixelSize();
-        this.ReDraw();
+        
         
         
 
     }
     
-    private void ReDraw()
-    {
-        Canvas.Children.Clear();
-        UpdateCanvasSize();
-        
-        for (int i = 0; i < height; i++)
-        {
-            for (int j = 0; j < width; j++)
-            {
-                //image[i, j] = 0;
-                this.DrawPixel(i, j);
-            }
-        }
-    }
     
     private async void Load(object? sender, RoutedEventArgs e)
     {
@@ -100,38 +76,13 @@ public partial class MainWindow : Window
         ReDraw();
     }
 
-    private void DrawPixel(int x, int y)
-    {
-        var pixel = new Avalonia.Controls.Shapes.Rectangle()
-        {
-            Width = pixelSize,
-            Height = pixelSize,
-            Fill = image[x, y] == 1 ? color : Brushes.White,
-        };
-        Canvas.SetLeft(pixel, y * pixelSize);
-        Canvas.SetTop(pixel, x * pixelSize);
-        Canvas.Children.Add(pixel);
-    }
     
     private void Close(object? sender, RoutedEventArgs e)
     {
         Environment.Exit(0);
     }
     
-    private void Press(object? sender, PointerPressedEventArgs e)
-    {
-        var position = e.GetPosition(Canvas);
-        int x = (int)(position.Y / pixelSize);
-        int y = (int)(position.X / pixelSize);
-
-        if (x >= 0 && x < height && y >= 0 && y < width)
-        {
-            image[x, y] = image[x, y] == 1 ? 0 : 1;
-            DrawPixel(x, y);
-        }
-        
-        
-    }
+    
     
     private void ChangeColor(object? sender, SelectionChangedEventArgs e)
     {
@@ -168,21 +119,14 @@ public partial class MainWindow : Window
         }
     }
 
-    private void OnSizeChanged(object sender, SizeChangedEventArgs e) {
+    private void OnSizeChanged(object sender, SizeChangedEventArgs e)
+    {
         TB1.Text = $"{this.Width} {this.Height}";
-        UpdatePixelSize();
-        ReDraw();    
+        Canvas.ChangeSize(this.Width, this.Height);
+        
     }
 
-    private void UpdatePixelSize() {
-        int maxPixelYSize = (int) (this.Height - 140) / height;
-        int maxPixelXSize = (int) (this.Width - 20) / width;
-        pixelSize = Math.Min(maxPixelXSize, maxPixelYSize);
-    }
-    private void UpdateCanvasSize() {
-        Canvas.Width = width * pixelSize;
-        Canvas.Height = height * pixelSize;
-    }
+    
 
 }
 
--- MyCanvas.axaml.cs
+++ MyCanvas.axaml.cs
@@ -1,7 +1,85 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Media;
 
 namespace AvaloniaApplication1;
 public class MyCanvas : Canvas
 {
-    
+    private IBrush? color = Brushes.Black;
+    private int[,] image;
+    private int imageWidth;
+    private int imageHeight;
+    private int pixelSize;
+
+    public MyCanvas()
+    {
+        imageWidth = 10;
+        imageHeight = 10;
+        pixelSize = 20;
+        image = new int[imageHeight, imageWidth];
+
+        UpdatePixelSize();
+        this.ReDraw();
+    }
+    private void ReDraw()
+    {
+        Children.Clear();
+        UpdateCanvasSize();
+        
+        for (int i = 0; i < imageHeight; i++)
+        {
+            for (int j = 0; j < imageWidth; j++)
+            {
+                //image[i, j] = 0;
+                this.DrawPixel(i, j);
+            }
+        }
+    }
+
+    private void DrawPixel(int x, int y)
+    {
+        var pixel = new Avalonia.Controls.Shapes.Rectangle()
+        {
+            Width = pixelSize,
+            Height = pixelSize,
+            Fill = image[x, y] == 1 ? color : Brushes.White,
+        };
+        SetLeft(pixel, y * pixelSize);
+        SetTop(pixel, x * pixelSize);
+        Children.Add(pixel);
+    }
+
+    private void Press(object? sender, PointerPressedEventArgs e)
+    {
+        var position = e.GetPosition(this);
+        int x = (int)(position.Y / pixelSize);
+        int y = (int)(position.X / pixelSize);
+
+        if (x >= 0 && x < imageHeight && y >= 0 && y < imageWidth)
+        {
+            image[x, y] = image[x, y] == 1 ? 0 : 1;
+            DrawPixel(x, y);
+        }
+        
+        
+    }
+
+
+    private void UpdatePixelSize()
+    {
+        int maxPixelYSize = (int) (this.Height - 140) / imageHeight;
+        int maxPixelXSize = (int) (this.Width - 20) / imageWidth;
+        pixelSize = Math.Min(maxPixelXSize, maxPixelYSize);
+    }
+    private void UpdateCanvasSize()
+    {
+        Width = imageWidth * pixelSize;
+        Height = imageHeight * pixelSize;
+    }
+    public void ChangeSize(double imageWidth, double imageHeight)
+    {
+        UpdatePixelSize();
+        ReDraw();    
+    }
 }
