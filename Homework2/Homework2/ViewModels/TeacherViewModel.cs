13b019ceaf3163a4d8fade4137214aebf3895249
Hannes Arthur Hohrein <hahoh24@student.sdu.dk>
2025-03-24 10:36:28 +0100

    delete subjects

--- Homework2/Homework2/ViewModels/TeacherViewModel.cs
+++ Homework2/Homework2/ViewModels/TeacherViewModel.cs
@@ -1,5 +1,6 @@
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows.Input;
 using Homework2.Classes;
 using ReactiveUI;
 
@@ -14,20 +15,47 @@ public class TeacherViewModel : ViewModelBase
         set => this.RaiseAndSetIfChanged(ref subjects, value);
     }
 
+    private Subject selectedSubject;
+    public Subject SelectedSubject
+    {
+        get => selectedSubject;
+        set => this.RaiseAndSetIfChanged(ref selectedSubject, value);
+    }
+
+    private User loggedInUser;
+    public User LoggedInUser
+    {
+        get => loggedInUser;
+        set => this.RaiseAndSetIfChanged(ref loggedInUser, value);
+    }
+    public ICommand DeleteSubjectsCommand { get; }
+
     public TeacherViewModel(User user)
     {
         Subjects = new ObservableCollection<Subject>();
-        this.MySubjects(user);
+        LoggedInUser = user;
+        this.MySubjects();
+        DeleteSubjectsCommand = new CommandHandler(DeleteSubject);
     }
 
-    public void MySubjects(User user)
+    public void MySubjects()
     {
         foreach (var e in SubjectHandler.GetSubjects())
         {
-            if (int.Parse(e.Value.TeacherId) == (user.Id))
+            if (int.Parse(e.Value.TeacherId) == (LoggedInUser.Id))
             {
                 Subjects.Add(e.Value);
             }
         }
+    }
+
+    public void DeleteSubject()
+    {
+        if (LoggedInUser != null && SelectedSubject != null)
+        {
+            SubjectHandler.DeleteSubject(SelectedSubject);
+        }
+        Subjects = new ObservableCollection<Subject>();
+        MySubjects();
     }
 }
