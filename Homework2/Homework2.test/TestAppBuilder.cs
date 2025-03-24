2a8b4d6d06b34cc2380265c5f48bb15cc94731ed
Hannes Arthur Hohrein <hahoh24@student.sdu.dk>
2025-03-24 11:58:46 +0100

    basic UITest framework

---
+++ Homework2/Homework2.test/TestAppBuilder.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Headless;
+using Avalonia;
+using Homework2;
+
+[assembly: AvaloniaTestApplication(typeof(TestAppBuilder))]
+
+public class TestAppBuilder
+{
+    public static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<App>()
+        .UseHeadless(new AvaloniaHeadlessPlatformOptions());
+}
---
+++ Homework2/Homework2.test/UITests.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia.Headless.XUnit;
+using Avalonia;
+using Homework2.Views;
+using Homework2.ViewModels;
+using Avalonia.Headless;
+
+public class UITests
+{
+    [AvaloniaFact]
+    public void ExampleTest()
+    {
+        var window = new MainWindow()
+        {
+            DataContext = new MainWindowViewModel()
+        };
+        window.Show();
+
+        Assert.NotNull(window);
+    }
+}
