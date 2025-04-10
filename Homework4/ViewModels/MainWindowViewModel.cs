6bc19d37c645ea7e9d16f51c7837baf68638ead0
fistr24@student.sdu.dk <fistr24@student.sdu.dk>
2025-04-10 09:08:18 +0200

    Initial mvvm commit

---
+++ Homework4/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Homework4.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ Homework4/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Homework4.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
